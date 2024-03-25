c19328fe256fdaa0ef3b10bc36920b6d60ff697f
goldenapple545 <egorvikturovgmail.com git config --global user.email egorvikturovgmail.com>
2024-03-25 16:22:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 
-# Приложение "Конвертер времени". Первая вкладка "Время" содержит две
-# кнопки: "Конвертировать" и "Сбросить", а также окно ввода текста для
-# ввода времени в секундах. Вторая вкладка "Формат" содержит две кнопки:
-# "Выбрать" и "Сбросить", и выпадающий список из 3 опций: "Секунды в
-# минуты", "Минуты в часы", "Часы в дни". Третья вкладка "Результат"
-# содержит результат обработки введенного текста из "Время" с учетом
-# опции, выбранной на "Формат". Формулы для работы с опциями: "Секунды
-# в минуты" - время / 60, "Минуты в часы" - время / 60, "Часы в дни" - время /
-# 24
-
 import PySimpleGUI as sg
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     # Создаем список элементов для первой вкладки
     tab1_layout = [
-        [sg.Text('Input time(sec):')],
+        [sg.Text('Input time:')],
         [sg.Input(key='-INPUT-')],
         [sg.Button('Convert')],
-        [sg.Button('Reset')]
+        [sg.Button('Reset', key='-RESET-')]
     ]
     # Создаем список элементов для второй вкладки
     tab2_layout = [
         [sg.Text('Choose convert to:')],
-        [sg.Combo(['Seconds in minutes', 'Minutes in hours', 'Hours in days'], key='-OPTION-')],
+        [sg.Combo(['Seconds in minutes', 'Minutes in hours', 'Hours in days'], key='-OPTION-', default_value='Seconds in minutes')],
         [sg.Button('Choose')],
-        [sg.Button('Reset')]
+        [sg.Button('Reset', key='-RESET-')]
     ]
     # Создаем список элементов для третьей вкладки
     tab3_layout = [
@@ -36,8 +24,8 @@ if __name__ == '__main__':
     ]
     # Создаем вкладки
     tab_group_layout = [
-        [sg.Tab('Time', tab1_layout), sg.Tab('Format', tab2_layout),
-         sg.Tab('Result', tab3_layout)]
+        [sg.Tab('Time', tab1_layout, key='-TAB1-'), sg.Tab('Format', tab2_layout),
+         sg.Tab('Result', tab3_layout, key='-TAB3-')]
     ]
     # Создаем окно приложения
     layout = [[sg.TabGroup(tab_group_layout)]]
@@ -51,11 +39,18 @@ if __name__ == '__main__':
         elif event == 'Convert':
             if values['-OPTION-'] == 'Seconds in minutes':
                 result = int(values['-INPUT-']) // 60
-            elif values['-OPTION-'] == 'Символы':
-                result = len(values['-INPUT-'])
-            elif values['-OPTION-'] == 'Строки':
-                result = len(values['-INPUT-'].split('\n'))
-            window['-RESULT-'].update(f'Количество: {result}')
+            elif values['-OPTION-'] == 'Minutes in hours':
+                result = int(values['-INPUT-']) // 60
+            elif values['-OPTION-'] == 'Hours in days':
+                result = int(values['-INPUT-']) // 24
+            window['-RESULT-'].update(f'Time: {result}')
+            window['-TAB3-'].select()
+        elif event == 'Choose':
+            window['-TAB1-'].select()
+        elif event == '-RESET-':
+            window['-RESULT-'].update(value='')
+            window.write_event_value('-INPUT-', '')
+            window['-INPUT-'].update(value='')
     window.close()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
