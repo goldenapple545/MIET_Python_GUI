53424503e5ee9b6e89137c169816292c859af48c
goldenapple545 <egorvikturovgmail.com git config --global user.email egorvikturovgmail.com>
2024-03-25 19:43:54 +0300

    separate main logic

--- main.py
+++ main.py
@@ -2,35 +2,36 @@
 
 import PySimpleGUI as sg
 
-if __name__ == '__main__':
-    # Создаем список элементов для первой вкладки
-    tab1_layout = [
-        [sg.Text('Input time:')],
-        [sg.Input(key='-INPUT-')],
-        [sg.Button('Convert')],
-        [sg.Button('Reset', key='-RESET-')]
-    ]
-    # Создаем список элементов для второй вкладки
-    tab2_layout = [
-        [sg.Text('Choose convert to:')],
-        [sg.Combo(['Seconds in minutes', 'Minutes in hours', 'Hours in days'], key='-OPTION-', default_value='Seconds in minutes')],
-        [sg.Button('Choose')],
-        [sg.Button('Reset', key='-RESET-')]
-    ]
-    # Создаем список элементов для третьей вкладки
-    tab3_layout = [
-        [sg.Text('Result:', size=(15, 1), justification='center')],
-        [sg.Text(size=(40, 1), key='-RESULT-')]
-    ]
-    # Создаем вкладки
-    tab_group_layout = [
-        [sg.Tab('Time', tab1_layout, key='-TAB1-'), sg.Tab('Format', tab2_layout),
-         sg.Tab('Result', tab3_layout, key='-TAB3-')]
-    ]
-    # Создаем окно приложения
-    layout = [[sg.TabGroup(tab_group_layout)]]
-    window = sg.Window('Time Converter', layout)
+# Создаем список элементов для первой вкладки
+tab1_layout = [
+    [sg.Text('Input time:')],
+    [sg.Input(key='-INPUT-')],
+    [sg.Button('Convert')],
+    [sg.Button('Reset', key='-RESET-')]
+]
+# Создаем список элементов для второй вкладки
+tab2_layout = [
+    [sg.Text('Choose convert to:')],
+    [sg.Combo(['Seconds in minutes', 'Minutes in hours', 'Hours in days'], key='-OPTION-',
+              default_value='Seconds in minutes')],
+    [sg.Button('Choose')],
+    [sg.Button('Reset', key='-RESET-')]
+]
+# Создаем список элементов для третьей вкладки
+tab3_layout = [
+    [sg.Text('Result:', size=(15, 1), justification='center')],
+    [sg.Text(size=(40, 1), key='-RESULT-')]
+]
+# Создаем вкладки
+tab_group_layout = [
+    [sg.Tab('Time', tab1_layout, key='-TAB1-'), sg.Tab('Format', tab2_layout),
+     sg.Tab('Result', tab3_layout, key='-TAB3-')]
+]
+# Создаем окно приложения
+layout = [[sg.TabGroup(tab_group_layout)]]
+window = sg.Window('Time Converter', layout)
 
+if __name__ == '__main__':
     # Обработка событий
     while True:
         event, values = window.read()
@@ -52,5 +53,3 @@ if __name__ == '__main__':
             window.write_event_value('-INPUT-', '')
             window['-INPUT-'].update(value='')
     window.close()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
